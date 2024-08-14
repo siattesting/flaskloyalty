2e319026795a7e2cf80c1d2da7fda62ac2307d55
siattesting <siat.testing@gmail.com>
2024-08-14 21:30:03 +0100

    initial

--- board/partners/partners.py
+++ board/partners/partners.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('partners', __name__, template_folder='templates')
 
 @bp.route("/partners")
 def partners():
-    return render_template("partners/partners.html")
+    search = request.args.get('q')
+    if search is not None:
+        partners_set = Partner.search(search)
+    else:
+        partners_set = Partner.query.all()
+    return render_template("partners/partners.html", partners=partners_set)
 
